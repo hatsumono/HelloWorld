4c3ddad9d1b08934a97294d2dce8cad66f99ae99
smyussuf <smyussuf@gmail.com>
2023-08-27 12:00:11 +0300

    Initial Commit

---
+++ library.py
@@ -0,0 +1,2 @@
+def gree():
+    print('Hello, World!')
---
+++ main.py
@@ -0,0 +1,8 @@
+from library import greet
+def main():
+    print('Hello,World')
+
+if __name__ == '__main__';
+
+    if __name__ '__main__':
+        main()
