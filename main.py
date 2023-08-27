9fd96fbe496bb71929d949ec58fa07e3c090b945
smyussuf <smyussuf@gmail.com>
2023-08-27 16:25:14 +0300

    Define and modify some variables

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from library import greet
 def main():
     print('Hello,World')
-    book = 'Dracula'
-    author = 'Brown Stroker'
-    release_year = 1897
-    goodreads_ratings = 4.01
+   # book = 'Dracula'
+   # author = 'Brown Stroker'
+   # release_year = 1897
+   # goodreads_ratings = 4.01
+
+    book,author,release_year,goodreads_ratings = 'Dracula','Brown Stroker','1897','4.01'
 
     print(book)
     print(author)
